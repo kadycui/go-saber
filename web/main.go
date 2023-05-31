0e0b305fb18d86f18e53c2f680765bc02b17dc80
kadycui <kadycui@qq.com>
2023-05-31 22:36:16 +0800

    ADD: HTTP 支持

---
+++ web/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"gostart/web/utils"
+	"net/http"
+)
+
+// 127.0.0.1:8080/server?server_id=1
+
+func main() {
+	http.HandleFunc("/server", utils.ServerHandler)
+	http.ListenAndServe(":8080", nil)
+}
